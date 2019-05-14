e7564c39215787c04c2c24f015f6e0d18105c210
DESKTOP-FC0MQRM\adamb <adamblatchley@hotmail.com>
2019-05-14 03:25:29 +0200

    fixed padding

--- tools/padding oracle attack/test.py
+++ tools/padding oracle attack/test.py
@@ -50,8 +50,9 @@ def oracle(message):
         print(r.text)
 
     return r.status_code
-
-inputstring = "2d00c696765ee44e54225a43b18126160b278ba99a45a58681444a02d01933881cb35ea63cd64837fa70dc3b77bef33181289de11317f3a5d8350b2c150c14f8"
+# a = b'\xdf@\xf4\xf1[\x9e\xa1\x16\xde\xe8\xa8\xb8U\x92\xd0\x0c\xfb`\x1d\x0e\xbe\x167\xb5\xcb#\xd7\xdbE\x99\xa5\xa4bMf\xf3\x8cQy3\xae\x07\xfd*\xe6]G\x92'
+# print(a.hex())
+inputstring = "f90b43afd31b037bc5b81371bd86d6aaf6523c146bab6f356f29e94e2d8391500c82fff0b2b774ec5d2aeda5f6c05c6be3f045e7c6afbfc52351752e1422f8c1"
 inputbytes = bytes.fromhex(inputstring)
 print(inputbytes)
 print(attack(inputbytes))
